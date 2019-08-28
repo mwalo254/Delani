888b8cd9489fbe861f19194d8ff3931ca6ab6ef2
mwalo254 <mwalonick@gmail.com>
2019-08-28 10:09:57 +0300

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+//UserInterface Logic
+$(document).ready(function(){
+    $("#design").click(function() {
+      $("#design").toggle();
+      $("#content5").toggle();
+    });
+  });
+    $(document).ready(function(){
+    $("#content5").click(function() {
+      $("#design").toggle();
+      $("#content5").toggle();
+    });
+  });
+   
+  $(document).ready(function(){
+    $("#dev").click(function() {
+      $("#dev").toggle();
+      $("#dev-content").toggle();
+    });
+  });
+    $(document).ready(function(){
+    $("#dev-content").click(function() {
+      $("#dev").toggle();
+      $("#dev-content").toggle();
+    });
+  });
+  $(document).ready(function(){
+    $("#product").click(function() {
+      $("#product").toggle();
+      $("#prod-content").toggle();
+    });
+  });
+    $(document).ready(function(){
+    $("#prod-content").click(function() {
+      $("#product").toggle();
+      $("#prod-content").toggle();
+    });
+  });
+  
